33495e1af449bc9a90bc135e06d18cac7957b2d9
Anuran Roy <anuranroy02@gmail.com>
2023-08-27 00:13:54 +0530

    [MODIFY] Tweaked Navbar components, as well as other minor CSS tweaks

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,24 @@
+import routes from "../routes/routes"
+import { toTitleCase } from "../utils/utils"
+
 export default function NavBar() {
     return (
-        <div className="navbar px-6 mx-6">
-            <h2 className="navbar-heading py-5">Navbar</h2>
-            <a href={"/notes"} className="navbar-item clickable w-100">Notes</a>
-            <div className="spacer-y"></div>
-            <a href={"/reminders"} className="navbar-item clickable w-100">Reminder</a>
-            <div className="spacer-y"></div>
-            <a href={"/trash"} className="navbar-item clickable w-100">Trash</a>
+        <div className="navbar flex-column px-6 mx-6">
+            <h2 className="navbar-heading py-5">DuxNotes</h2>
+            <div id="navbar-options py-5">
+                {
+                    Object.keys(routes).map((route: string) => {
+                        return route.length > 1 ? (
+                            <>
+                                <div className="spacer-y"></div>
+                                <a href={route} className="navbar-item clickable w-100">{toTitleCase(route.substring(1))}</a>
+                            </>) : <></>
+                    })
+                }
+            </div>
+            <div id="footer" className="w-100 my-4 bottom-0 py-4 center text-white">
+                Made by <a className="no-decoration" href="https://github.com/anuran-roy"><b>Anuran</b></a> with ❤
+            </div>
         </div>
     )
 }
