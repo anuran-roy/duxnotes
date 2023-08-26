91d304a35a0d188b0b69954d2d51f03c5cd05dda
Anuran Roy <anuranroy02@gmail.com>
2023-08-26 23:38:55 +0530

    [MODIFY] Added basic tweak functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -144,3 +144,9 @@ export const getContentPreview = (content: string) => {
     }
     return preview;
 }
+
+export const toTitleCase = (str: string) => {
+    return str.toLowerCase().split(' ').map(function(word: string) {
+        return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+}
